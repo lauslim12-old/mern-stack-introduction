7e19ff8d05138d1b427447c358245809d7ce81d4
lauslim12 <nicholasdwiarto@yahoo.com>
2020-06-08 12:10:29 +0700

    Added React Router for the Header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,32 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
 import logo from '../images/logo.svg';
 
 class Header extends React.Component {
   render() {
     return (
-      <nav id="navigation" className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item active">
-              <a className="nav-link" href="index.html">Nicholas Dwiarto <span className="sr-only">(current)</span></a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <Router>
+        <nav id="navigation" className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <img src={logo} className="App-logo" alt="logo" />
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <Link to="/" className="nav-link">Nicholas Dwiarto <span className="sr-only">(current)</span></Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/create" className="nav-link">Create A Todo</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <Route path="/" exact component={TodosList} />
+        <Route path="/edit/:id" component={EditTodo} />
+        <Route path="/create" component={CreateTodo} />
+      </Router>
     )
   }
 }
