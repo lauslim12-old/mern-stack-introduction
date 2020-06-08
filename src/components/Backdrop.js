f100ee6e12a9aae7d7ad0417544d64d9ae2bb38c
lauslim12 <nicholasdwiarto@yahoo.com>
2020-06-08 11:57:02 +0700

    Changed React's starter page to be a Backdrop component.

---
+++ src/components/Backdrop.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class Backdrop extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://nicholasdw.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Nicholas Dwiarto's here.
+          </a>
+        </header>
+      </div>
+    )
+  }
+}
+
+export default Backdrop;
