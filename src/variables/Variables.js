2c1815169fe2b833785cd073c383e60f0fdd200b
lauslim12 <nicholasdwiarto@yahoo.com>
2020-06-09 16:50:46 +0700

    Added new variable for the DELETE API endpoint.

--- src/variables/Variables.js
+++ src/variables/Variables.js
@@ -2,3 +2,4 @@ export const BASEURL = `http://localhost:8000`;
 export const READURL = `${BASEURL}/todos`;
 export const CREATEURL = `${BASEURL}/todos/create`;
 export const UPDATEURL = `${BASEURL}/todos/update`;
+export const DELETEURL = `${BASEURL}/todos/delete`;
