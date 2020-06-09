c2379f31f3808cf45fade60763310e9623bac5b2
lauslim12 <nicholasdwiarto@yahoo.com>
2020-06-09 20:24:58 +0700

    Added Helmet and Compression for production server.

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const compression = require('compression');
+const helmet = require('helmet');
 const Database = require('./configurations/Database');
 const PORT = 8000;
 
@@ -15,6 +17,8 @@ db.synchronizeDatabase();
 // Setting
 app.use(cors());
 app.use(bodyParser.json());
+app.use(compression());
+app.use(helmet());
 app.use('/todos', todoRoutes);
 
 app.listen(PORT, function() {
