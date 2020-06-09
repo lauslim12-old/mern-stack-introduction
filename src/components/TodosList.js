24d422f41b394cacd41473391e74551e12d4003d
lauslim12 <nicholasdwiarto@yahoo.com>
2020-06-09 15:40:47 +0700

    Finished the READ part of the application.

---
+++ src/components/TodosList.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit, faTrash, faCheck } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+import { READURL } from './../variables/Variables';
+
+const Todo = (props) => (
+  <tr>
+    {!props.todo.todoCompleted && <td></td>}
+    {props.todo.todoCompleted && (
+      <td>
+        <FontAwesomeIcon icon={faCheck} />
+      </td>
+    )}
+    <td>{props.todo.todoTitle}</td>
+    <td>{props.todo.todoResponsibility}</td>
+    <td>{props.todo.todoPriority}</td>
+    <td>
+      <Link to={"/edit/" + props.todo.id}>
+        <FontAwesomeIcon icon={faEdit} className="App-table-icon" />
+      </Link>
+      <Link to={"/delete/" + props.todo.id}>
+        <FontAwesomeIcon icon={faTrash} className="App-table-icon" />
+      </Link>
+    </td>
+  </tr>
+);
+
+class TodosList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: []
+    };
+  }
+
+  fetchData() {
+    axios.get(READURL)
+      .then(res => {
+        this.setState({
+          todos: res.data
+        });
+        //console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  todoTable() {
+    return this.state.todos.map(function(currentTodo, i) {
+      return <Todo todo={currentTodo} key={i} />;
+    });
+  }
+
+  render() {
+    return (
+      <div className="App-container">
+        <h3 className="App-title">Todo List</h3>
+
+        <table className="table table-dark table-striped mt-3 text-center">
+          <thead>
+            <tr>
+              <td>Status</td>
+              <td>Title</td>
+              <td>Responsibility</td>
+              <td>Priority</td>
+              <td>Action</td>
+            </tr>
+          </thead>
+          <tbody>
+            { this.todoTable() }
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+}
+
+export default TodosList;
