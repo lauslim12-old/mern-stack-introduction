116d708c23cec9f7a23a6e7fe389f8bf9734bbbf
lauslim12 <nicholasdwiarto@yahoo.com>
2020-09-21 10:14:31 +0700

    Fix: Polished the URL to be a single URL.

--- src/variables/Variables.js
+++ src/variables/Variables.js
@@ -1,5 +1,2 @@
 export const BASEURL = `${process.env.REACT_APP_API}`;
-export const READURL = `${BASEURL}/todos`;
-export const CREATEURL = `${BASEURL}/todos/create`;
-export const UPDATEURL = `${BASEURL}/todos/update`;
-export const DELETEURL = `${BASEURL}/todos/delete`;
+export const ENDPOINTURL = `${BASEURL}/todos`;
