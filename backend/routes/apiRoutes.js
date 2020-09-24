4dc81916c61e13ffa6e1e864b873246130ea2f3a
lauslim12 <nicholasdwiarto@yahoo.com>
2020-09-24 14:14:09 +0700

    Refactor: Refactored routes.

--- backend/routes/apiRoutes.js
+++ backend/routes/apiRoutes.js
@@ -2,10 +2,15 @@ const express = require('express');
 const TodoController = require('../controllers/TodoController');
 const todoRoutes = express.Router();
 
-todoRoutes.route('/').get(TodoController.getAllTodos);
-todoRoutes.route('/:id').get(TodoController.getTodo);
-todoRoutes.route('/create').post(TodoController.addTodo);
-todoRoutes.route('/update/:id').put(TodoController.updateTodo);
-todoRoutes.route('/delete/:id').delete(TodoController.deleteTodo);
+todoRoutes
+  .route('/')
+  .get(TodoController.getAllTodos)
+  .post(TodoController.addTodo);
+
+todoRoutes
+  .route('/:id')
+  .get(TodoController.getTodo)
+  .put(TodoController.updateTodo)
+  .delete(TodoController.deleteTodo);
 
 module.exports = todoRoutes;
